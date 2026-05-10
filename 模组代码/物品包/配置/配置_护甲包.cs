7e31cd1341039add4f7861e654c5a7d0af59e431
fI8YGFSOcaTYxhhuu3 <252904806+fI8YGFSOcaTYxhhuu3@users.noreply.github.com>
2026-05-10 12:23:16 -0700

    添加护甲包的单件和套装加成开关配置

--- 模组代码/物品包/配置/配置_护甲包.cs
+++ 模组代码/物品包/配置/配置_护甲包.cs
@@ -1,4 +1,8 @@
+using MultipleArmorSetsFramework;
 using System.ComponentModel;
+using Terraria;
+using Terraria.ModLoader.Config;
+using 物品包.玩家;
 
 namespace 物品包.配置;
 
@@ -14,5 +18,18 @@ public class 类型_配置_护甲包 : 类型_配置_物品包 {
 
     [DefaultValue( 10 )]
     public override int 行数 { get; set; }
+
+    [Header( "功能设置" )]
+
+    [DefaultValue( true )]
+    public bool 启用单件加成 { get; set; }
+
+    [DefaultValue( true )]
+    public bool 启用套装加成 { get; set; }
+
+    public override void OnChanged() {
+        if ( Main.LocalPlayer == null || !Main.LocalPlayer.active ) return;
+        Main.LocalPlayer.GetModPlayer<类型_玩家_护甲包>().注册护甲();
+    }
 
 }
