a1dd622b79794921cb53382ceb79bcf726350e0e
fI8YGFSOcaTYxhhuu3 <252904806+fI8YGFSOcaTYxhhuu3@users.noreply.github.com>
2026-05-12 13:55:55 -0700

    移除 界面_包槽位_物品缓存

--- 模组代码/物品包/界面/界面_包槽位_嵌套.cs
+++ 模组代码/物品包/界面/界面_包槽位_嵌套.cs
@@ -6,11 +6,13 @@ namespace 物品包.界面;
 
 
 
-public class 类型_包槽位_嵌套( 类型_缓存包_物品 所属包, int 槽位索引 ) : 类型_包槽位_物品缓存( 所属包, 槽位索引 ) {
+public class 类型_包槽位_嵌套( 类型_嵌套包 所属包, int 槽位索引 ) : 类型_包槽位_物品( 所属包, 槽位索引 ) {
 
-    public override void RightClick( UIMouseEvent 鼠标事件 ) {
-        if ( 所属物品.ModItem is 类型_物品包 目标包 ) { 类型_系统_界面管理.界面管理器.切换窗口( 目标包 ); return; }
-        base.RightClick( 鼠标事件 );
+    internal override void LeftClick_交换物品() {
+        base.LeftClick_交换物品();
+        所属包.脏标记 = true;
     }
+
+    public override void RightClick( UIMouseEvent 鼠标事件 ) { if ( !所属物品.IsAir ) 类型_系统_界面管理.界面管理器.切换窗口( ( 类型_物品包 ) 所属物品.ModItem ); }
 
 }
--- 模组代码/物品包/界面/界面_包槽位_饰品.cs
+++ 模组代码/物品包/界面/界面_包槽位_饰品.cs
@@ -6,10 +6,11 @@ namespace 物品包.界面;
 
 
 
-public class 类型_包槽位_饰品( 类型_缓存包_物品 所属包, int 槽位索引 ) : 类型_包槽位_物品缓存( 所属包, 槽位索引 ) {
+public class 类型_包槽位_饰品( 类型_饰品包 所属包, int 槽位索引 ) : 类型_包槽位_物品( 所属包, 槽位索引 ) {
 
     internal override void LeftClick_交换物品() {
         base.LeftClick_交换物品();
+        所属包.脏标记 = true;
         Main.LocalPlayer.GetModPlayer<类型_玩家_饰品包>().脏标记_额外缓存 = true;
     }
 
