db7c8767b53f1ef22f1333a0e3b35fcb9813ce51
fI8YGFSOcaTYxhhuu3 <252904806+fI8YGFSOcaTYxhhuu3@users.noreply.github.com>
2026-05-19 15:44:27 -0700

    令护甲包和饰品包玩家使用独立配置

--- 模组代码/物品包/玩家/玩家_护甲包.cs
+++ 模组代码/物品包/玩家/玩家_护甲包.cs
@@ -1,55 +1,79 @@
 using MultipleArmorSetsFramework;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 using 物品包.Items;
-using 物品包.配置;
 
 namespace 物品包.玩家;
 
 
 
 // 特征成员
-public partial class 类型_玩家_护甲包 : 类型_玩家_缓存包_额外缓存<类型_护甲包, 类型_护甲组合> {
-    public 类型_配置_护甲包 配置 = new();
+public partial class 类型_玩家_护甲包 : 类型_玩家_同步缓存包<类型_护甲包, 结构_同步数据_护甲包> {
+    private readonly List<string> 注册备份列表 = [];
 
-    public void 注册护甲() { Player.GetModPlayer<护甲玩家>().护甲管理器.注册( "物品包", 缓存列表_额外缓存, new( 配置.单件加成模式, 配置.套装加成模式 ) ); }
+    public void 注册护甲() {
+        var 护甲框架 = Player.GetModPlayer<护甲玩家>().护甲管理器;
+
+        foreach ( var 注册ID in CollectionsMarshal.AsSpan( 注册备份列表 ) ) 护甲框架.注销( 注册ID );
+        注册备份列表.Clear();
+
+        foreach ( var 数据 in CollectionsMarshal.AsSpan( 缓存列表_同步缓存 ) ) {
+            string 标识ID = 数据.ID.ToString();
+            护甲框架.注册( 标识ID, 数据.数据列表, new( 数据.功能配置.单件加成模式, 数据.功能配置.套装加成模式 ) );
+            注册备份列表.Add( 标识ID );
+        }
+    }
 }
 
 // 简单重写成员
-public partial class 类型_玩家_护甲包 : 类型_玩家_缓存包_额外缓存<类型_护甲包, 类型_护甲组合> {
+public partial class 类型_玩家_护甲包 {
     public override 枚举_物品包类型 缓存包标识 => 枚举_物品包类型.护甲包;
 }
 
 // 特征重写函数
-public partial class 类型_玩家_护甲包 : 类型_玩家_缓存包_额外缓存<类型_护甲包, 类型_护甲组合> {
-    protected override void 脏标记更新_额外缓存() { base.脏标记更新_额外缓存(); 注册护甲(); }
-    public override void OnEnterWorld() { base.OnEnterWorld(); 配置 = ( 类型_配置_护甲包 ) ModContent.GetInstance<类型_配置_护甲包>().Clone(); }
+public partial class 类型_玩家_护甲包 {
+    protected override void 脏标记更新_同步缓存() { base.脏标记更新_同步缓存(); 注册护甲(); }
 }
 
 // 网络同步
-public partial class 类型_玩家_护甲包 : 类型_玩家_缓存包_额外缓存<类型_护甲包, 类型_护甲组合> {
+public partial class 类型_玩家_护甲包 {
     public override void 网络发送( int 接收玩家ID, int 发送玩家ID ) {
         ModPacket 网络数据 = Mod.GetPacket();
         网络数据.Write( ( byte ) 类型_物品包模组.枚举_同步对象类型.护甲包 );
         网络数据.Write( ( byte ) Player.whoAmI );
-        网络数据.Write( 缓存列表_额外缓存.Count );
-        foreach ( var 组合 in 缓存列表_额外缓存 ) {
-            ItemIO.Send( 组合.头盔, 网络数据, writeStack: false, writeFavorite: false );
-            ItemIO.Send( 组合.胸甲, 网络数据, writeStack: false, writeFavorite: false );
-            ItemIO.Send( 组合.护腿, 网络数据, writeStack: false, writeFavorite: false );
+
+        网络数据.Write( 缓存列表_同步缓存.Count );
+        foreach ( var 数据 in CollectionsMarshal.AsSpan( 缓存列表_同步缓存 ) ) {
+            网络数据.Write( 数据.ID.ToByteArray() );
+            网络数据.Write( ( byte ) 数据.功能配置.单件加成模式 );
+            网络数据.Write( ( byte ) 数据.功能配置.套装加成模式 );
+
+            网络数据.Write( 数据.数据列表.Count );
+            foreach ( var 组合 in CollectionsMarshal.AsSpan( 数据.数据列表 ) ) {
+                ItemIO.Send( 组合.头盔, 网络数据, writeStack: false, writeFavorite: false );
+                ItemIO.Send( 组合.胸甲, 网络数据, writeStack: false, writeFavorite: false );
+                ItemIO.Send( 组合.护腿, 网络数据, writeStack: false, writeFavorite: false );
+            }
         }
         网络数据.Send( 接收玩家ID, 发送玩家ID );
     }
     public override void 网络接收( BinaryReader 网络流 ) {
-        缓存列表_额外缓存.Clear();
-        int 数量 = 网络流.ReadInt32();
-        for ( int i = 0; i < 数量; i++ ) {
-            Item 头盔 = ItemIO.Receive( 网络流, readStack: false, readFavorite: false );
-            Item 胸甲 = ItemIO.Receive( 网络流, readStack: false, readFavorite: false );
-            Item 护腿 = ItemIO.Receive( 网络流, readStack: false, readFavorite: false );
-            缓存列表_额外缓存.Add( new 类型_护甲组合( 头盔, 胸甲, 护腿 ) );
+        缓存列表_同步缓存.Clear();
+        int 包数量 = 网络流.ReadInt32();
+        for ( int i = 0; i < 包数量; i++ ) {
+            结构_同步数据_护甲包 数据 = new() {
+                ID = new System.Guid( 网络流.ReadBytes( 16 ) ),
+                功能配置 = new() { 单件加成模式 = ( 枚举_加成模式 ) 网络流.ReadByte(), 套装加成模式 = ( 枚举_加成模式 ) 网络流.ReadByte() },
+                数据列表 = []
+            };
+
+            int 组合数量 = 网络流.ReadInt32();
+            for ( int j = 0; j < 组合数量; j++ ) 数据.数据列表.Add( new( ItemIO.Receive( 网络流, readStack: false, readFavorite: false ), ItemIO.Receive( 网络流, readStack: false, readFavorite: false ), ItemIO.Receive( 网络流, readStack: false, readFavorite: false ) ) );
+            缓存列表_同步缓存.Add( 数据 );
         }
         注册护甲();
     }
--- 模组代码/物品包/玩家/玩家_饰品包.cs
+++ 模组代码/物品包/玩家/玩家_饰品包.cs
@@ -4,55 +4,62 @@ using Terraria;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 using 物品包.Items;
-using 物品包.配置;
 
 namespace 物品包.玩家;
 
 
 
-// 特征成员
-public partial class 类型_玩家_饰品包 : 类型_玩家_缓存包_额外缓存<类型_饰品包, Item> {
-    public 类型_配置_饰品包 配置 = new();
-}
-
 // 简单重写成员
-public partial class 类型_玩家_饰品包 : 类型_玩家_缓存包_额外缓存<类型_饰品包, Item> {
+public partial class 类型_玩家_饰品包 : 类型_玩家_同步缓存包<类型_饰品包, 结构_同步数据_饰品包> {
     public override 枚举_物品包类型 缓存包标识 => 枚举_物品包类型.饰品包;
 }
 
 // 特征重写函数
-public partial class 类型_玩家_饰品包 : 类型_玩家_缓存包_额外缓存<类型_饰品包, Item> {
-    public override void OnEnterWorld() { base.OnEnterWorld(); 配置 = ( 类型_配置_饰品包 ) ModContent.GetInstance<类型_配置_饰品包>().Clone(); }
+public partial class 类型_玩家_饰品包 {
     public override void UpdateEquips() { base.UpdateEquips(); 应用缓存列表加成(); }
     public override void UpdateVisibleAccessories() { base.UpdateVisibleAccessories(); 应用缓存列表视效(); }
 }
 
 // 网络同步
-public partial class 类型_玩家_饰品包 : 类型_玩家_缓存包_额外缓存<类型_饰品包, Item> {
+public partial class 类型_玩家_饰品包 {
     public override void 网络发送( int 接收玩家ID, int 发送玩家ID ) {
         ModPacket 网络数据 = Mod.GetPacket();
         网络数据.Write( ( byte ) 类型_物品包模组.枚举_同步对象类型.饰品包 );
         网络数据.Write( ( byte ) Player.whoAmI );
-        网络数据.Write( 缓存列表_额外缓存.Count );
-        foreach ( var 饰品 in 缓存列表_额外缓存 ) ItemIO.Send( 饰品, 网络数据, writeStack: false, writeFavorite: false );
+        网络数据.Write( 缓存列表_同步缓存.Count );
+        foreach ( var 数据 in 缓存列表_同步缓存 ) {
+            网络数据.Write( 数据.功能配置.应用前缀效果 );
+            网络数据.Write( 数据.功能配置.应用视觉效果 );
+            网络数据.Write( 数据.数据列表.Count ); foreach ( var 饰品 in CollectionsMarshal.AsSpan( 数据.数据列表 ) ) ItemIO.Send( 饰品, 网络数据, writeStack: false, writeFavorite: false );
+        }
         网络数据.Send( 接收玩家ID, 发送玩家ID );
     }
     public override void 网络接收( BinaryReader 网络流 ) {
-        缓存列表_额外缓存.Clear();
-        int 数量 = 网络流.ReadInt32();
-        for ( int i = 0; i < 数量; i++ ) 缓存列表_额外缓存.Add( ItemIO.Receive( 网络流, readStack: false, readFavorite: false ) );
+        缓存列表_同步缓存.Clear();
+        int 包数量 = 网络流.ReadInt32();
+        for ( int i = 0; i < 包数量; i++ ) {
+            结构_同步数据_饰品包 数据 = new() {
+                功能配置 = new() { 应用前缀效果 = 网络流.ReadBoolean(), 应用视觉效果 = 网络流.ReadBoolean() },
+                数据列表 = []
+            };
+
+            int 饰品数量 = 网络流.ReadInt32();
+            for ( int j = 0; j < 饰品数量; j++ ) 数据.数据列表.Add( ItemIO.Receive( 网络流, readStack: false, readFavorite: false ) );
+            缓存列表_同步缓存.Add( 数据 );
+        }
     }
 }
 
 // 辅助函数
-public partial class 类型_玩家_饰品包 : 类型_玩家_缓存包_额外缓存<类型_饰品包, Item> {
-    private void 应用饰品加成( Item 饰品 ) {
-        Player.ApplyEquipFunctional( 饰品, true );
-        Player.GrantArmorBenefits( 饰品 );
-        if ( 配置.应用视觉效果 ) Player.ApplyEquipVanity( 饰品 );
-        if ( 配置.应用前缀效果 ) Player.GrantPrefixBenefits( 饰品 );
-        饰品.ModItem?.UpdateEquip( Player );
+public partial class 类型_玩家_饰品包 {
+    private void 应用缓存列表加成() {
+        foreach ( var 数据 in CollectionsMarshal.AsSpan( 缓存列表_同步缓存 ) ) foreach ( var 饰品 in CollectionsMarshal.AsSpan( 数据.数据列表 ) ) {
+            Player.ApplyEquipFunctional( 饰品, true );
+            Player.GrantArmorBenefits( 饰品 );
+            if ( 数据.功能配置.应用视觉效果 ) Player.ApplyEquipVanity( 饰品 );
+            if ( 数据.功能配置.应用前缀效果 ) Player.GrantPrefixBenefits( 饰品 );
+            饰品.ModItem?.UpdateEquip( Player );
+        }
     }
-    private void 应用缓存列表加成() { foreach ( var 饰品 in CollectionsMarshal.AsSpan( 缓存列表_额外缓存 ) ) 应用饰品加成( 饰品 ); }
-    private void 应用缓存列表视效() { if ( 配置.应用视觉效果 ) foreach ( var 饰品 in CollectionsMarshal.AsSpan( 缓存列表_额外缓存 ) ) Player.UpdateVisibleAccessory( 13, 饰品, true ); }
+    private void 应用缓存列表视效() { foreach ( var 数据 in CollectionsMarshal.AsSpan( 缓存列表_同步缓存 ) ) if ( 数据.功能配置.应用视觉效果 ) foreach ( var 饰品 in CollectionsMarshal.AsSpan( 数据.数据列表 ) ) Player.UpdateVisibleAccessory( 13, 饰品, true ); }
 }
