aa519274e258a20aebca21af466a899919c6bb35
fI8YGFSOcaTYxhhuu3 <252904806+fI8YGFSOcaTYxhhuu3@users.noreply.github.com>
2026-05-19 17:35:38 -0700

    模板错误修复

--- 模组代码/物品包/Items/类型_缓存包_可切换.cs
+++ 模组代码/物品包/Items/类型_缓存包_可切换.cs
@@ -17,11 +17,11 @@ public partial interface 接口_缓存包_可切换 : 接口_缓存包 {
 }
 
 // 特征重写函数
-public partial interface 接口_缓存包_可切换 : 接口_缓存包 {
-    void 接口_物品包.SaveData( TagCompound 存档标签 ) { SaveData_物品包( 存档标签 ); 存档标签[ "启用状态" ] = 启用状态; }
-    void 接口_物品包.LoadData( TagCompound 存档标签 ) { LoadData_缓存包( 存档标签 ); 启用状态 = 存档标签.GetBool( "启用状态" ); }
-    void 接口_物品包.NetSend( BinaryWriter 网络流 ) { NetSend_物品包( 网络流 ); 网络流.Write( 启用状态 ); }
-    void 接口_物品包.NetReceive( BinaryReader 网络流 ) { NetReceive_缓存包( 网络流 ); 启用状态 = 网络流.ReadBoolean(); }
+public partial interface 接口_缓存包_可切换<类型_缓存数据, 泛型_配置, 泛型_玩家, 泛型_包槽位> : 接口_缓存包<类型_缓存数据, 泛型_配置, 泛型_玩家, 泛型_包槽位> where 泛型_配置 : 类型_配置_物品包 where 泛型_玩家 : 类型_玩家_物品包 where 泛型_包槽位 : 类型_包槽位_物品 {
+    void 接口_物品包<泛型_配置, 泛型_玩家, 泛型_包槽位>.SaveData( TagCompound 存档标签 ) { SaveData_物品包( 存档标签 ); 存档标签[ "启用状态" ] = 启用状态; }
+    void 接口_物品包<泛型_配置, 泛型_玩家, 泛型_包槽位>.LoadData( TagCompound 存档标签 ) { LoadData_缓存包( 存档标签 ); 启用状态 = 存档标签.GetBool( "启用状态" ); }
+    void 接口_物品包<泛型_配置, 泛型_玩家, 泛型_包槽位>.NetSend( BinaryWriter 网络流 ) { NetSend_物品包( 网络流 ); 网络流.Write( 启用状态 ); }
+    void 接口_物品包<泛型_配置, 泛型_玩家, 泛型_包槽位>.NetReceive( BinaryReader 网络流 ) { NetReceive_缓存包( 网络流 ); 启用状态 = 网络流.ReadBoolean(); }
 }
 
 // 特征重写函数
@@ -36,7 +36,7 @@ public partial interface 接口_缓存包_可切换<类型_缓存数据, 泛型_
     void 接口_缓存包_可切换.切换启用状态_缓存包_可切换() {
         启用状态 = !启用状态;
         脏标记 = true;
-        if ( 玩家 is 类型_玩家_缓存包_非模板基类 缓存玩家 ) 缓存玩家.脏标记_缓存包 = true;
+        if ( 玩家 is 类型_玩家_缓存包 缓存玩家 ) 缓存玩家.脏标记_缓存包 = true;
         Terraria.Audio.SoundEngine.PlaySound( Terraria.ID.SoundID.MenuTick );
     }
 }
