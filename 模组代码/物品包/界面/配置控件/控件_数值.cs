92bb9c7b7b58be94dac0ad5cdd112276dd89e18e
fI8YGFSOcaTYxhhuu3 <252904806+fI8YGFSOcaTYxhhuu3@users.noreply.github.com>
2026-05-21 07:29:45 -0700

    调整数值滑块

--- 模组代码/物品包/界面/配置控件/控件_数值.cs
+++ 模组代码/物品包/界面/配置控件/控件_数值.cs
@@ -23,6 +23,9 @@ public class 类型_控件_数值 : UIElement {
     private readonly int 最大值;
 
     private readonly UIElement 滑块区域;
+    private readonly UITextPanel<string> 减号按钮;
+    private readonly UITextPanel<string> 加号按钮;
+
     private bool 拖拽状态 = false;
 
     public 类型_控件_数值( Action<int> 设置器, int 初始数值, int 最小值, int 最大值 ) {
@@ -34,12 +37,12 @@ public class 类型_控件_数值 : UIElement {
         Width.Set( 控件长度, 0f ); Height.Set( 30f, 0f );
         SetPadding( 0 );
 
-        UITextPanel<string> 减号按钮 = new( "-" ) { TextHAlign = 0.5f, VAlign = 0.5f, BackgroundColor = new( 73, 94, 171 ) };
+        减号按钮 = new( "-" ) { TextHAlign = 0.5f, VAlign = 0.5f, BackgroundColor = new( 73, 94, 171 ) };
         减号按钮.Left.Set( 0f, 0f ); 减号按钮.Width.Set( 按钮大小, 0f ); 减号按钮.Height.Set( 按钮大小, 0f );
         减号按钮.OnLeftClick += ( evt, el ) => { 设置器( int.Max( --当前数值, 最小值 ) ); SoundEngine.PlaySound( SoundID.MenuTick ); };
         Append( 减号按钮 );
 
-        UITextPanel<string> 加号按钮 = new( "+" ) { TextHAlign = 0.5f, VAlign = 0.5f, BackgroundColor = new( 73, 94, 171 ) };
+        加号按钮 = new( "+" ) { TextHAlign = 0.5f, VAlign = 0.5f, BackgroundColor = new( 73, 94, 171 ) };
         加号按钮.Left.Set( 控件长度 - 按钮大小, 0f ); 加号按钮.Width.Set( 按钮大小, 0f ); 加号按钮.Height.Set( 按钮大小, 0f );
         加号按钮.OnLeftClick += ( evt, el ) => { 设置器( int.Min( ++当前数值, 最大值 ) ); SoundEngine.PlaySound( SoundID.MenuTick ); };
         Append( 加号按钮 );
@@ -47,7 +50,6 @@ public class 类型_控件_数值 : UIElement {
         滑块区域 = new() { VAlign = 0.5f };
         滑块区域.Left.Set( 按钮大小, 0f ); 滑块区域.Width.Set( 滑块长度, 0f ); 滑块区域.Height.Set( 按钮大小, 0f );
         滑块区域.OnLeftMouseDown += ( evt, el ) => 拖拽状态 = true;
-        滑块区域.OnLeftMouseUp += ( evt, el ) => 拖拽状态 = false;
         Append( 滑块区域 );
     }
 
@@ -64,7 +66,10 @@ public class 类型_控件_数值 : UIElement {
 
     public override void Update( GameTime gameTime ) {
         base.Update( gameTime );
-        if ( 拖拽状态 ) 更新滑块拖拽();
+        if ( 拖拽状态 ) {
+            if ( !Main.mouseLeft ) 拖拽状态 = false;
+            else 更新滑块拖拽();
+        }
     }
 
     public override void Draw( SpriteBatch spriteBatch ) {
@@ -81,6 +86,6 @@ public class 类型_控件_数值 : UIElement {
         Rectangle 游标矩形 = new( ( int ) ( 区域.X + 区域.Width * 比例 - 4 ), ( int ) 区域.Y + 2, 8, 20 );
         spriteBatch.Draw( TextureAssets.MagicPixel.Value, 游标矩形, Color.White );
 
-        if ( 滑块区域.IsMouseHovering || 拖拽状态 ) Utils.DrawBorderString( spriteBatch, 当前数值.ToString(), new( Main.mouseX, Main.mouseY - 20 ), Color.White, 0.8f );
+        if ( IsMouseHovering || 拖拽状态 ) Utils.DrawBorderString( spriteBatch, 当前数值.ToString(), new( Main.mouseX, Main.mouseY - 20 ), Color.White, 0.8f );
     }
 }
